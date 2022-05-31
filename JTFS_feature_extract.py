cd73c90eb4a1e7d35e0981de0d74d781ba24578d
Changhong Wang <changhong.wang@qmul.ac.uk>
2022-05-31 11:31:27 +0200

    Create JTFS_feature_extract.py

---
+++ JTFS_feature_extract.py
@@ -0,0 +1,14 @@
+import time
+import subprocess
+import warnings
+warnings.filterwarnings('ignore')
+
+# extract dJTFS-avg feature in matlab
+t0 = time.time()
+subprocess.call(["matlab",
+                 "-r",
+                 "JTFS_feature_extract",
+                 "-nodisplay",
+                 "-nodesktop"])
+
+print('Feature extraction time:%.2f hours.' % ((time.time() - t0)/3600))
